b12ad9a01fb846484bfd357c9fda06f2824fa496
Serendipityzhezi <1342558165@qq.com>
2024-06-06 14:22:55 +0800

    feat：😄添加ExecuteMessage实体类，存放进程结果信息

---
+++ src/main/java/com/yupi/yojcodesandbox/model/ExecuteMessage.java
@@ -0,0 +1,14 @@
+package com.yupi.yojcodesandbox.model;
+
+import lombok.Data;
+
+@Data
+public class ExecuteMessage {
+
+    private Integer exitValue;
+
+    private String message;
+
+    private String errorMessage;
+
+}
