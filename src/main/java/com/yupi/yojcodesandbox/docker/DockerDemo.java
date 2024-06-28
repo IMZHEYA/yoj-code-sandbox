987e290a137c2aade83268063fc62f7d83e211dd
Serendipityzhezi <1342558165@qq.com>
2024-06-28 21:34:41 +0800

    feat：😄Java操作Docker下载镜像

--- src/main/java/com/yupi/yojcodesandbox/docker/DockerDemo.java
+++ src/main/java/com/yupi/yojcodesandbox/docker/DockerDemo.java
@@ -1,13 +1,26 @@
 package com.yupi.yojcodesandbox.docker;
 
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.command.PingCmd;
+import com.github.dockerjava.api.command.PullImageCmd;
+import com.github.dockerjava.api.command.PullImageResultCallback;
+import com.github.dockerjava.api.model.PullResponseItem;
 import com.github.dockerjava.core.DockerClientBuilder;
 
 public class DockerDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        // 1 下载镜像
         DockerClient dockerClient = DockerClientBuilder.getInstance().build();
-        PingCmd pingCmd = dockerClient.pingCmd();
-        pingCmd.exec();
+        String image = "nginx:latest";
+        PullImageCmd pullImageCmd = dockerClient.pullImageCmd(image);
+        PullImageResultCallback pullImageResultCallback = new PullImageResultCallback(){
+            @Override
+            public void onNext(PullResponseItem item) {
+                System.out.printf("下载镜像:" + item.getStatus());
+                super.onNext(item);
+            }
+        };
+        pullImageCmd.exec(pullImageResultCallback).awaitCompletion();
+
     }
+
 }
