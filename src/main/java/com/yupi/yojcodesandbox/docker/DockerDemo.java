122a437f7fba0165b01e5f7b77f6c1d2bf9aec97
Serendipityzhezi <1342558165@qq.com>
2024-06-28 21:37:40 +0800

    feat：😄Java操作Docker-查看日志

--- src/main/java/com/yupi/yojcodesandbox/docker/DockerDemo.java
+++ src/main/java/com/yupi/yojcodesandbox/docker/DockerDemo.java
@@ -3,8 +3,10 @@ package com.yupi.yojcodesandbox.docker;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.*;
 import com.github.dockerjava.api.model.Container;
+import com.github.dockerjava.api.model.Frame;
 import com.github.dockerjava.api.model.PullResponseItem;
 import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.command.LogContainerResultCallback;
 
 import java.util.List;
 
@@ -37,6 +39,15 @@ public class DockerDemo {
         }
         //4 启动容器
         dockerClient.startContainerCmd(containId).exec();
+        //5 查看日志
+        LogContainerResultCallback logContainerResultCallback = new LogContainerResultCallback(){
+            @Override
+            public void onNext(Frame item) {
+                System.out.println(item.getStreamType());  //返回日志帧的流类型，表示日志消息的来源
+                System.out.println("日志：" + new String(item.getPayload()));//返回日志帧的实际日志内容，返回类型为字节数组
+                super.onNext(item);
+            }
+        };
 
     }
 
