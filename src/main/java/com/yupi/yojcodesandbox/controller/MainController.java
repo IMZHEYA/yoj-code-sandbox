d8ff8c1de91ab613d9193f250e2ee939a7997fb8
Serendipityzhezi <1342558165@qq.com>
2024-07-03 15:46:53 +0800

    feat：😃新增鉴权请求头和密钥，添加基本API认证

--- src/main/java/com/yupi/yojcodesandbox/controller/MainController.java
+++ src/main/java/com/yupi/yojcodesandbox/controller/MainController.java
@@ -9,12 +9,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController("/")
 public class MainController {
     @Resource
     private JavaNativeCodeSandbox javaNativeCodeSandbox;
-
+    //鉴权请求头
+    public static final String AUTH_REQUEST_HEADER = "auth";
+    //密钥
+    public static final String AUTH_REQUEST_SECRET = "secretKey";
     @GetMapping("/health")
     public String healthCheck() {
         return "ok";
@@ -28,7 +33,13 @@ public class MainController {
     //http://localhost:8090/health
     //http://localhost:8090/executeCode
     @PostMapping("/executeCode")
-    public ExecutecodeResponse executeCode(@RequestBody ExecutecodeCodeRequest executecodeCodeRequest) {
+    public ExecutecodeResponse executeCode(@RequestBody ExecutecodeCodeRequest executecodeCodeRequest, HttpServletRequest request, HttpServletResponse response) {
+        //基本的API认证
+        String authHeader = request.getHeader(AUTH_REQUEST_HEADER);
+        if(!authHeader.equals(AUTH_REQUEST_SECRET)){
+            response.setStatus(403);
+            return null;
+        }
         if(executecodeCodeRequest == null){
             throw new RuntimeException("请求参数为空");
         }
